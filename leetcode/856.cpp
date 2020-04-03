69392b61b3093f19a8fd2022748cc98b6d455226
YoonYeoHwan <johnyhgmg@kookmin.ac.kr>
2020-04-03 10:03:33 +0900

    Add 856.cpp

---
+++ leetcode/856.cpp
@@ -0,0 +1,24 @@
+// 856. Score of Parentheses
+
+class Solution {
+public:
+    int scoreOfParentheses(string S) {
+        stack<char> s;
+        int score = 0;
+        int tmp = 0;
+        for(int i=0; i<S.size(); i++) {
+            if(S[i] == '(') {
+                s.push(S[i]);
+                tmp++;
+            }
+            else {
+                if(s.top() == '(') {
+                    score += pow(2, tmp - 1);
+                }
+                tmp--;
+                s.push(S[i]);
+            }
+        }
+        return score;
+    }
+};
